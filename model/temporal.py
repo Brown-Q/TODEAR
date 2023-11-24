36b51bd6aef6d809e7963b1c9d98ce6d94e7bce1
qianye001 <48096514+Brown-Q@users.noreply.github.com>
2023-11-24 10:51:46 +0800

    Update temporal.py

--- model/temporal.py
+++ model/temporal.py
@@ -4,7 +4,7 @@ import operator
 import json
 
 
-def dataloader(file,entity_file, relation_file, time_file,valid_file, test_file):
+def dataloader(file, entity_file, relation_file, time_file, valid_file, test_file):
     entity_dict = {}
     relation_dict = {}
     time_dict = {}
@@ -14,52 +14,56 @@ def dataloader(file,entity_file, relation_file, time_file,valid_file, test_file)
             entity, embedding = line.strip().split('\t')
             embedding = np.array(json.loads(embedding))
             entity_dict[entity] = embedding
+
     with open(relation_file, 'r', encoding='utf-8') as r_f:
         lines = r_f.readlines()
         for line in lines:
             relation, embedding = line.strip().split('\t')
             embedding = np.array(json.loads(embedding))
             relation_dict[relation] = embedding
+
     with open(time_file, 'r', encoding='utf-8') as t_f:
         lines = t_f.readlines()
         for line in lines:
             time, embedding = line.strip().split('\t')
             embedding = np.array(json.loads(embedding))
             time_dict[time] = embedding
+
     with open(test_file, 'r', encoding='utf-8') as test_f:
         lines = test_f.readlines()
         for line in lines:
             quadruple = line.strip().split('\t')
+
             s_ = quadruple[0]
             if s_ not in entity_dict:
-                entity_dict[s_] = np.random.uniform(-6 / math.sqrt(80), 6 / math.sqrt(80),80)
+                entity_dict[s_] = np.random.uniform(-6 / math.sqrt(100), 6 / math.sqrt(100), 100)
             r_ = quadruple[1]
             if r_ not in relation_dict:
                 relation_dict[r_] = np.random.uniform(-6 / math.sqrt(100), 6 / math.sqrt(100), 100)
             o_ = quadruple[2]
             if o_ not in entity_dict:
-                entity_dict[o_] = np.random.uniform(-6 / math.sqrt(80), 6 / math.sqrt(80), 80)
+                entity_dict[o_] = np.random.uniform(-6 / math.sqrt(100), 6 / math.sqrt(100), 100)
             t_ = quadruple[3]
             if t_ not in time_dict:
-                time_dict[t_] = np.random.uniform(-6 / math.sqrt(20), 6 / math.sqrt(20), 20)
+                time_dict[t_] = np.random.uniform(-6 / math.sqrt(100), 6 / math.sqrt(100), 100)
                 
     with open(valid_file, 'r', encoding='utf-8') as test_f:
         lines = test_f.readlines()
         for line in lines:
+            quadruple = line.strip().split('\t')
             if len(quadruple) == 4:
-                quadruple = line.strip().split('\t')
                 s_ = quadruple[0]
                 if s_ not in entity_dict:
-                    entity_dict[s_] = np.random.uniform(-6 / math.sqrt(80), 6 / math.sqrt(80),80)
+                    entity_dict[s_] = np.random.uniform(-6 / math.sqrt(100), 6 / math.sqrt(100), 100)
                 r_ = quadruple[1]
                 if r_ not in relation_dict:
                     relation_dict[r_] = np.random.uniform(-6 / math.sqrt(100), 6 / math.sqrt(100), 100)
                 o_ = quadruple[2]
                 if o_ not in entity_dict:
-                    entity_dict[o_] = np.random.uniform(-6 / math.sqrt(80), 6 / math.sqrt(80), 80)
+                    entity_dict[o_] = np.random.uniform(-6 / math.sqrt(100), 6 / math.sqrt(100), 100)
                 t_ = quadruple[3]
                 if t_ not in time_dict:
-                    time_dict[t_] = np.random.uniform(-6 / math.sqrt(20), 6 / math.sqrt(20), 20)
+                    time_dict[t_] = np.random.uniform(-6 / math.sqrt(100), 6 / math.sqrt(100), 100)
                 
     entity_dict = dict(sorted(entity_dict.items(),key = lambda x:int(x[0])))
     relation_dict = dict(sorted(relation_dict.items(),key = lambda x:int(x[0])))
@@ -71,7 +75,7 @@ def dataloader(file,entity_file, relation_file, time_file,valid_file, test_file)
             f_e.write(str(list(entity_dict[e])))
             f_e.write("\n")
         for j in range(500):
-            padding = np.random.uniform(-6 / math.sqrt(80), 6 / math.sqrt(80), 80) 
+            padding = np.random.uniform(-6 / math.sqrt(100), 6 / math.sqrt(100), 100)
             f_e.write(str(int(e)+1) + "\t")
             f_e.write(str(list(padding)))
             f_e.write("\n")
@@ -98,21 +102,15 @@ def dataloader(file,entity_file, relation_file, time_file,valid_file, test_file)
             f_t.write(str(list(time_dict[t])))
             f_t.write("\n") 
         for j in range(500):      
-            padding = np.random.uniform(-6 / math.sqrt(20), 6 / math.sqrt(20), 20) 
+            padding = np.random.uniform(-6 / math.sqrt(100), 6 / math.sqrt(100), 100)
             f_t.write(str(int(t)+1) + "\t")
             f_t.write(str(list(padding)))
             f_t.write("\n")    
-    # embeddings = np.empty(shape=[0, 80])       
-    # with open(file+"res/entity_emb.txt", 'r', encoding='utf-8') as e_f:
-    #     lines = e_f.readlines()
-    #     for line in lines:
-    #         entity, embedding = line.strip().split('\t')
-    #         embedding = np.array([json.loads(embedding)])
-    #         embeddings = np.concatenate([embeddings,embedding],axis=0)
-    # print(embeddings)               
+        
     return entity_dict, relation_dict, time_dict           
                 
-                
+    
+            
 if __name__ == '__main__':
     file = "../data/ICEWS14/"
     _, _, train_quadruple = dataloader(file,file+"res/entity_50dim_batch400.txt",file+"res/relation_50dim_batch400.txt",file+"res/time_50dim_batch400.txt",file+"valid.txt",file+"test.txt")
